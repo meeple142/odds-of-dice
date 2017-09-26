5f406a04e337dc802be5b802743a425535c0dcdc
Joshua McKinney <mckinneyj@byui.edu>
2017-09-25 23:28:00 -0600

    inital commit

---
+++ odds-of-dice.js
@@ -0,0 +1,32 @@
+/*eslint-env node*/
+/*eslint no-unused-vars:0, no-console:0*/
+var fs = require('fs');
+var sidesOfDice = +process.argv[2];
+var numOfDice = +process.argv[3];
+var filename = process.argv[4]
+var max = Math.pow(sidesOfDice, numOfDice);
+var rolls = [];
+var text;
+console.log("max:", max);
+
+for (i = 0; i < max; ++i) {
+    //make the base
+    text = i.toString(sidesOfDice);
+
+    //add 0 to the front
+    while (text.length < numOfDice) {
+        text = "0" + text;
+    }
+
+    rolls.push(text.split('')
+        .sort()
+        .join('-'))
+
+}
+var str = rolls
+    .sort()
+    .join('\n');
+//var str = rolls.join('\n');
+//var count = str.match(/^.*(\d)-\1-(\d)-\2-\2.*$/gm).length
+//console.log(count + '/' + max + '=' + (count / max * 100) + '%');
+fs.writeFileSync(filename + ".txt", rolls.join('\n'), 'utf8');
